93086b330c2a13a56523d8c73a7ac1a08311fdf1
yvitoriano <160539397+yvitoriano@users.noreply.github.com>
2025-09-15 17:21:59 +0000

    Adicionando uma rota

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/')
 def index():
     return 'PSI COM ALBA'
-
+@app.route('/contato')
+def contato():
+    return 'yvitoriano@ifrn.edu.br'
 if __name__ == '__main__':
     app.run()
