7615e62b1fc29d93427f16af8f16dd34a4230411
yvitoriano <160539397+yvitoriano@users.noreply.github.com>
2025-09-16 20:38:39 +0000

    adicionando html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,12 @@ def index():
     return 'PSI COM ALBA'
 @app.route('/contato')
 def contato():
-    return 'yvitoriano@ifrn.edu.br'
+    return '<h1>yvitoriano@ifrn.edu.br</h1>   <h3>testessss</h3>'
+@app.route('/exemplo')
+def exemplo():
+    return render_template('teste1.html')
+@app.route('/exemplo2')
+def exemplo2():
+    return render_template('teste2.html')
 if __name__ == '__main__':
     app.run()
